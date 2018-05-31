94ce6ea3c0a0c7827629b0ba55c2fa787de28b47
ericcalabretta <eric.calabretta@gmail.com>
2018-05-31 12:59:06 -0500

    demo

--- controls/hab_pkg_deps_example.rb
+++ controls/hab_pkg_deps_example.rb
@@ -1,28 +1,11 @@
-control 'Mongo Deps' do
-  impact 0.7
-  title 'np-mongodb'
-  desc 'Checks the dependencies of the np-mongodb running service'
-  describe hab_pkg_deps(url: 'http://localhost', name: 'np-mongodb', group: 'default', pkg_name: 'glibc') do
-    its('origin') { should eq 'core' }
-    its('release') { should eq '20170513201042' }
-    its('version') { should eq '2.22' }
-  end
-end
 
-control 'Tomcat Deps' do
+control 'Tomcat Deps tomcat8' do
   impact 0.7
   title 'national-parks'
   desc 'Checks the dependencies of the np-mongodb running service'
   describe hab_pkg_deps(url: 'http://localhost', name: 'national-parks', group: 'default', pkg_name: 'tomcat8') do
     its('origin') { should eq 'core' }
-    its('release') { should eq '20170514144202' }
+    its('release') { should eq  '20170514144202-wrong' }
     its('version') { should eq '8.5.9' }
   end
-end
-
-
-describe hab_pkg_deps(url: 'http://localhost', name: 'national-parks', group: 'default', pkg_name: 'fake-pkg-name') do
-  its('origin') { should eq 'core' }
-  its('release') { should eq '20170514144202' }
-  its('version') { should eq '8.5.9' }
 end
