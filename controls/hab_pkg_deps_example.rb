2546826acda8fbe0080497d1c24a9211e6188567
ericcalabretta <eric.calabretta@gmail.com>
2018-12-07 15:33:16 -0600

    sync

--- controls/hab_pkg_deps_example.rb
+++ controls/hab_pkg_deps_example.rb
@@ -1,10 +1,21 @@
-control 'Tomcat Deps tomcat8' do
+control 'dependency_acl' do
   impact 1.0
-  title 'national-parks'
-  desc 'Checks the dependencies of the np-mongodb running service'
-  describe hab_pkg_deps(url: 'http://localhost', name: 'national-parks', group: 'default', pkg_name: 'glibc') do
+  title 'acl'
+  desc 'Checks the dependencies of the national-park running service'
+  describe hab_pkg_deps(url: 'http://localhost', name: 'national-parks', group: 'dev', pkg_name: 'acl') do
     its('origin') { should eq 'core' }
-    its('version') { should eq  '2.22' }
-    its('release') { should be >= 20170513201042 }
+    its('version') { should eq  '2.2.52' }
+    its('release') { should be >= 20170513213108 }
   end
 end
+
+# control 'dependency_attr' do
+#   impact 1.0
+#   title 'attr'
+#   desc 'Checks the dependencies of the national-park running service'
+#   describe hab_pkg_deps(url: 'http://54.200.140.118', name: 'national-parks', group: 'dev', pkg_name: 'acl') do
+#     its('origin') { should eq 'core' }
+#     its('version') { should eq  '2.2.52' }
+#     its('release') { should be >= 20170513213108 }
+#   end
+# end
