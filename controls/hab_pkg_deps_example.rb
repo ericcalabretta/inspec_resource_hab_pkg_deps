ac60e221f8aac1e46e70aa92ac8cb4237e404bcc
ericcalabretta <eric.calabretta@gmail.com>
2018-12-09 22:14:49 -0600

    updated example and title

--- controls/hab_pkg_deps_example.rb
+++ controls/hab_pkg_deps_example.rb
@@ -1,21 +1,10 @@
-control 'dependency_acl' do
+control 'CVE-2017-16997' do
   impact 1.0
-  title 'acl'
+  title 'glibc'
   desc 'Checks the dependencies of the national-park running service'
-  describe hab_pkg_deps(url: 'http://localhost', name: 'national-parks', group: 'default', pkg_name: 'acl') do
+  describe hab_pkg_deps(url: 'http://localhost', name: 'national-parks', group: 'default', pkg_name: 'glibc') do
     its('origin') { should eq 'core' }
-    its('version') { should eq  '2.2.52' }
-    its('release') { should be >= 20170513213108 }
+    its('version') { should eq  '2.27' }
+    its('release') { should be >= 20180608041157 }
   end
 end
-
-# control 'dependency_attr' do
-#   impact 1.0
-#   title 'attr'
-#   desc 'Checks the dependencies of the national-park running service'
-#   describe hab_pkg_deps(url: 'http://54.200.140.118', name: 'national-parks', group: 'dev', pkg_name: 'acl') do
-#     its('origin') { should eq 'core' }
-#     its('version') { should eq  '2.2.52' }
-#     its('release') { should be >= 20170513213108 }
-#   end
-# end
