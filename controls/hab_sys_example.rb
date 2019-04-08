618c6ed7d3dfb6d2f5abb6aa8abb7883f02da25f
ericcalabretta <eric.calabretta@gmail.com>
2019-04-08 13:55:28 -0500

    nginx

--- controls/hab_sys_example.rb
+++
@@ -1,8 +0,0 @@
-control 'hab system information' do
-    impact 1.0
-    title 'hab sys'
-    desc 'Checks the habitat version is the expected version'
-    describe hab_sys(url: attribute('url'), name: attribute('name'), group: attribute('group')) do
-      its('hab_version') { should eq '0.74.0/20190204225911' }
-    end
-  end
