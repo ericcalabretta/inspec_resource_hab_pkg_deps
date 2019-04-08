8c1680df7bd05f18b02da89cea672607ab259223
ericcalabretta <eric.calabretta@gmail.com>
2019-04-08 11:11:18 -0500

    updates

--- controls/hab_pkg_example.rb
+++ controls/hab_pkg_example.rb
@@ -1,10 +1,10 @@
-control 'CVE-2017-16997' do
+control 'Deployment mismatch' do
     impact 1.0
-    title 'glibc'
-    desc 'Checks the dependencies of the national-park running service'
+    title 'confirm deployment 20190303202613'
+    desc 'Confirms the exact expected version of the package is running'
     describe hab_pkg(url: attribute('url'), name: attribute('name'), group: attribute('group')) do
       its('pkg_ident') { should eq 'eric/sample/0.1.0/20190303202613' }
-      its('pkg_svc_user') { should eq 'root' }
+      its('pkg_svc_user') { should eq 'hab' }
       its('pkg_svc_group') { should eq 'hab' }
     end
   end
--- controls/hab_sys_example.rb
+++ controls/hab_sys_example.rb
@@ -1,7 +1,7 @@
-control 'hab sys' do
+control 'hab system information' do
     impact 1.0
     title 'hab sys'
-    desc 'Checks the dependencies of the national-park running service'
+    desc 'Checks the habitat version is the expected version'
     describe hab_sys(url: attribute('url'), name: attribute('name'), group: attribute('group')) do
       its('hab_version') { should eq '0.74.0/20190204225911' }
     end
