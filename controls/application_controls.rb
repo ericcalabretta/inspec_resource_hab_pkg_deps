4931d2b158dc6e3bc809c0b0e15f8202f5707aaa
ericcalabretta <eric.calabretta@gmail.com>
2019-04-08 21:52:54 -0500

    updated

--- controls/application_controls.rb
+++ controls/application_controls.rb
@@ -1,6 +1,6 @@
 name = attribute('name', default:'contosouniversity')
 group = attribute('group', default:'default')
-url = attribute('url', default:'http://18.237.80.22')
+url = attribute('url', default:'http://192.168.50.121')
 
 control 'Health Check' do
   impact 1.0
@@ -16,7 +16,7 @@ control 'Habitat Version' do
     title 'Habitat Version 78'
     desc 'Checks the habitat version is the expected version'
     describe hab_sys(url: attribute('url'), name: attribute('name'), group: attribute('group')) do
-      its('hab_version') { should eq '0.78.0/20190313123140' }
+      its('hab_version') { should eq '0.78.0/20190313123218' }
     end
   end
 
@@ -38,6 +38,6 @@ control 'Deployed Release' do
     title 'Current Release 20190314030908'
     desc 'Confirms the exact expected version of the package is running'
     describe hab_pkg(url: attribute('url'), name: attribute('name'), group: attribute('group')) do
-      its('pkg_ident') { should eq 'eric/nginx/1.15.8/20190314030908' }
+      its('pkg_ident') { should eq 'eric/contosouniversity/1.2.8/20190303163830' }
     end
   end
